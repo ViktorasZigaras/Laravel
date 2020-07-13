e26f9c17536dfcc90a3422f0ca1d4404ec61e12c
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-07-13 12:26:29 +0300

    init

---
+++ app/Http/Controllers/ByeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ByeController extends Controller
+{
+    public function doneMethod() : string {
+        return 'method';
+    }
+}
